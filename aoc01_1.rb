2cb060435be0903ccc726fefa9ed8faa53fe0b29
Alex Antsiferov <wetterkrank@gmail.com>
2021-12-01 09:50:47 +0100

    looks nicer this way

--- aoc01_1.rb
+++ aoc01_1.rb
@@ -1,9 +1,9 @@
 # https://adventofcode.com/2021/day/1
 # Count times when next depth measurement is larger than the previous
 
-data = File.open('aoc01_input.txt').read.lines(chomp: true).map(&:to_i)
+data = ::File.open('aoc01_input.txt').read.lines(chomp: true).map(&:to_i)
 
 count = 0
-data.each_cons(2) { |pair| count += 1 if pair[1] > pair[0] }
+data.each_cons(2) { |(previous, current)| count += 1 if current > previous }
 
 puts count
--- aoc01_2.rb
+++ aoc01_2.rb
@@ -1,15 +1,9 @@
 # https://adventofcode.com/2021/day/2
 # Count times when the next sliding window's sum is larger than the previous'
 
-data = File.open('aoc01_input.txt').read.lines(chomp: true).map(&:to_i)
+data = ::File.open('aoc01_input.txt').read.lines(chomp: true).map(&:to_i)
 
 count = 0
-previous = data[0..2].sum
-
-data.each_cons(3) do |window|
-  sum = window.sum
-  count += 1 if sum > previous
-  previous = sum
-end
+data.each_cons(3).each_cons(2) { |(prev, cur)| count += 1 if cur.sum > prev.sum }
 
 puts count

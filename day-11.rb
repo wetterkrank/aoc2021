bbe6c95831efcecac636a6498bf56843d5e10f78
Alex Antsiferov <wetterkrank@gmail.com>
2021-12-11 22:54:41 +0100

    cellular automaton with recursion; used more convenient structure for grid

--- day-11.rb
+++ day-11.rb
@@ -6,32 +6,6 @@ require_relative 'common'
 DIRS = [-1, 0, 1].repeated_permutation(2).to_a
 
 class Day11 < AdventDay
-  def cycle(grid, step: nil)
-    y = grid.size
-    x = grid.first.size
-    y.times { |j| x.times { |i| grid[j][i] += 1 } }
-    y.times { |j| x.times { |i| flash(grid, j, i) } }
-    return step + 1 if step
-  end
-
-  def count_flashes(grid)
-    grid.reduce(0) { |acc, elem| acc + elem.count(0) }
-  end
-
-  def flash(grid, y, x)
-    return 0 if grid[y][x] <= 9
-
-    grid[y][x] = 0
-    DIRS.sum do |dy, dx|
-      xn = x + dx
-      yn = y + dy
-      next 0 if xn.negative? || yn.negative? || xn >= grid.first.size || yn >= grid.size
-
-      grid[yn][xn] += 1 unless grid[yn][xn].zero?
-      flash(grid, yn, xn)
-    end
-  end
-
   def first_part
     grid = input
     100.times.sum do 
@@ -42,7 +16,6 @@ class Day11 < AdventDay
 
   def second_part
     grid = input
-
     (1..).find do 
       cycle(grid)
       count_flashes(grid) == 100
@@ -51,8 +24,33 @@ class Day11 < AdventDay
 
   private
 
+  def cycle(grid, step: nil)
+    grid.each_key { |coords| grid[coords] += 1 }
+    grid.each_key { |coords| flash(grid, *coords) }
+    return step + 1 if step
+  end
+
+  def flash(grid, x, y)
+    return 0 if grid[[x, y]].nil? || grid[[x, y]] <= 9
+
+    grid[[x, y]] = 0
+    DIRS.sum do |dx, dy|
+      adjacent = [x + dx, y + dy]
+      grid[adjacent] += 1 unless grid[adjacent].nil? || grid[adjacent].zero?
+      flash(grid, *adjacent)
+    end
+  end
+
+  def count_flashes(grid)
+    grid.reduce(0) { |acc, elem| acc + elem.count(0) }
+  end
+
   def convert_data(data)
-    super.map { |line| line.chars.map(&:to_i) }
+    super.each_with_object({}).with_index do |(line, obj), j|
+      line.chars.each_with_index do |char, i|
+        obj[[j, i]] = char.to_i
+      end
+    end
   end
 end
 

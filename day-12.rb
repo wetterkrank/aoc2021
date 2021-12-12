3acf6177eccdc10a7d23d9fdecb8f5b84f0f1c55
Alex Antsiferov <wetterkrank@gmail.com>
2021-12-12 16:52:30 +0100

    graph paths, finally!

--- day-12.rb
+++ day-12.rb
@@ -1,44 +1,41 @@
+# https://adventofcode.com/2021/day/12
+# Passage Pathing, counting graph paths
+
 require_relative 'common'
 
 class Day12 < AdventDay
-
-  def stop?(node, path)
-    node.chars.first > 'Z' && path.include?(node)
-  end
-
-  def find_paths(adj, current, path, paths, &block)
-    path << current
-    puts "Return: #{path}\n" if current == 'end'
-    return paths << path if current == 'end'
-    
-    next_nodes = adj[current].dup
-    next_nodes.reject! { |node| yield(node, path) unless node == 'end' }
-    
-    # puts "Path: #{path}"
-    # puts "#{current} -> #{next_nodes} (rejected: #{adj[current] - next_nodes})\n"
-
-    next_nodes.each do |node|
-      find_paths(adj, node, path.dup, paths, &block)
-    end
-    paths.count
-  end
-
   def first_part
-    find_paths(input, 'start', [], []) { |node, path| node.chars.first > 'Z' && path.include?(node) }
+    count_paths(input, 'start', {})
   end
-
+  
   def second_part
+    # Extra condition -- allow only one "small" cave visited twice
+    constraint = ->(node, path) { path[node] == 1 && path.values.none? { |val| val == 2 } }
+    count_paths(input, 'start', {}, constraint)
   end
 
   private
+
+  def big?(node)
+    node.chars.first <= 'Z'
+  end
+
+  def count_paths(adj, curr, visited, constraint = nil)
+    return 1 if curr == 'end'
+
+    visited[curr] = visited[curr].nil? ? 1 : visited[curr] + 1 unless big?(curr)
+    # Allow big caves and small caves not visited yet + extra condition if given
+    next_nodes = adj[curr].filter do |node|
+      node != 'start' && (visited[node].nil? || big?(node) || constraint&.call(node, visited))
+    end
+    next_nodes.sum { |node| count_paths(adj, node, visited.dup, constraint) }
+  end
 
   def convert_data(data)
     pair = super.map { _1.split('-') }
     pair.each_with_object({}) do |(a, b), map|
       map[a] = map[a].nil? ? [b] : map[a] << b
-      # unless %w[start end].include? a
-        map[b] = map[b].nil? ? [a] : map[b] << a
-      # end
+      map[b] = map[b].nil? ? [a] : map[b] << a
     end
   end
 end

72003616244da81f90588e429fb2066037605924
Alex Antsiferov <wetterkrank@gmail.com>
2021-12-12 11:16:21 +0100

    day 12 first part

--- day-12.rb
+++ day-12.rb
@@ -1,7 +1,30 @@
 require_relative 'common'
 
 class Day12 < AdventDay
+
+  def stop?(node, path)
+    node.chars.first > 'Z' && path.include?(node)
+  end
+
+  def find_paths(adj, current, path, paths, &block)
+    path << current
+    puts "Return: #{path}\n" if current == 'end'
+    return paths << path if current == 'end'
+    
+    next_nodes = adj[current].dup
+    next_nodes.reject! { |node| yield(node, path) unless node == 'end' }
+    
+    # puts "Path: #{path}"
+    # puts "#{current} -> #{next_nodes} (rejected: #{adj[current] - next_nodes})\n"
+
+    next_nodes.each do |node|
+      find_paths(adj, node, path.dup, paths, &block)
+    end
+    paths.count
+  end
+
   def first_part
+    find_paths(input, 'start', [], []) { |node, path| node.chars.first > 'Z' && path.include?(node) }
   end
 
   def second_part
@@ -10,7 +33,13 @@ class Day12 < AdventDay
   private
 
   def convert_data(data)
-    super
+    pair = super.map { _1.split('-') }
+    pair.each_with_object({}) do |(a, b), map|
+      map[a] = map[a].nil? ? [b] : map[a] << b
+      # unless %w[start end].include? a
+        map[b] = map[b].nil? ? [a] : map[b] << a
+      # end
+    end
   end
 end
 

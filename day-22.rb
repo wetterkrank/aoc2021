d2c7209ad444307c7e43010703552311f635387a
Alex Antsiferov <wetterkrank@gmail.com>
2021-12-22 07:24:34 +0100

    day 22, part 1 only

--- day-22.rb
+++ day-22.rb
@@ -2,6 +2,21 @@ require_relative 'common'
 
 class Day22 < AdventDay
   def first_part
+    grid = input.each_with_object({}) do |instruction, obj|
+      ranges, setting = instruction
+      next if ranges.any? { |range| range.first < -50 || range.first > 50 }
+
+      puts "Ranges: #{ranges}, setting: #{setting}"
+      xr, yr, zr = ranges
+      xr.each do |x|
+        yr.each do |y|
+          zr.each do |z|
+            obj[[x, y, z]] = setting
+          end
+        end
+      end
+    end
+    puts(grid.count { |k, v| v == true })
   end
 
   def second_part
@@ -10,7 +25,11 @@ class Day22 < AdventDay
   private
 
   def convert_data(data)
-    super
+    super.map do |line| 
+      setting, ranges = line.split 
+      axes = ranges.split(',').map { eval(_1[2..]) }
+      [axes, setting == 'on']
+    end
   end
 end
 
